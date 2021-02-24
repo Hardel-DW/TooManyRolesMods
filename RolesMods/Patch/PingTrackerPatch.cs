17e80351b8542cf5d34d261291f82d1f4410663a
Hardel <42413417+Hardel-DW@users.noreply.github.com>
2021-02-24 03:22:23 +0100

    Create PingTrackerPatch.cs

---
+++ RolesMods/Patch/PingTrackerPatch.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace RolesMods.Patch {
+
+    [HarmonyPatch(typeof(PingTracker), nameof(PingTracker.Update))]
+    [HarmonyPriority(Priority.First)]
+    public static class PingTrackerPatch {
+        private static Vector3 lastDist = Vector3.zero;
+
+        public static void Postfix(ref PingTracker __instance) {
+            if (!GlobalVariable.isGameStarted) {
+                AspectPosition aspect = __instance.text.gameObject.GetComponent<AspectPosition>();
+                if (aspect.DistanceFromEdge != lastDist) {
+                    aspect.DistanceFromEdge += new Vector3(0.6f, 0);
+                    aspect.AdjustPosition();
+
+                    lastDist = aspect.DistanceFromEdge;
+                }
+                __instance.text.Text += $"\n[2294E6FF]Too Many Roles Mods[] \ndiscord.gg/AP9axbXXNC";
+            }
+        }
+    }
+}
