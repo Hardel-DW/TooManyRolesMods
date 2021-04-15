6fdd91ba65e1229f73118cb5eeba5c7988441c6e
Hardel <42413417+Hardel-DW@users.noreply.github.com>
2021-04-16 00:39:33 +0200

    Intro Cut Scene fix

--- RolesMods/Utility/CustomRoles/Patch/IntroCutScene.cs
+++ RolesMods/Utility/CustomRoles/Patch/IntroCutScene.cs
@@ -3,19 +3,19 @@ using UnityEngine;
 
 namespace RolesMods.Utility.CustomRoles.Patch {
 
-    [HarmonyPatch(typeof(IntroCutscene), nameof(IntroCutscene.CoBegin))]
+    [HarmonyPatch(typeof(IntroCutscene.Nested_0), nameof(IntroCutscene.Nested_0.MoveNext))]
     public static class IntroCutScenePatch {
-        public static void Postfix(IntroCutscene __instance) {
+        public static void Postfix(IntroCutscene.Nested_0 __instance) {
             byte localPlayerId = PlayerControl.LocalPlayer.PlayerId;
             bool isImpostor = PlayerControl.LocalPlayer.Data.IsImpostor;
 
             foreach (var Role in RoleManager.AllRoles) {
                 if (Role.HasRole(localPlayerId) && Role.ShowIntroCutScene) {
-                    __instance.Title.text = Role.Name;
-                    __instance.Title.m_fontScale /= 1 + (Mathf.Max(0f, Role.Name.Length - 7) * (1 / 3));
-                    __instance.Title.color = Role.Color;
-                    __instance.ImpostorText.text = Role.IntroDescription;
-                    __instance.BackgroundBar.material.color = Role.Color;
+                    __instance.__this.Title.text = Role.Name;
+                    __instance.__this.Title.m_fontScale /= 1 + (Mathf.Max(0f, Role.Name.Length - 7) * (1 / 3));
+                    __instance.__this.Title.color = Role.Color;
+                    __instance.__this.ImpostorText.text = Role.IntroDescription;
+                    __instance.__this.BackgroundBar.material.color = Role.Color;
                 }
             }
         }
